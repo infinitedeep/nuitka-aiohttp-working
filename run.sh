acf4ff311db43db11fdd363159c1dd479baf2a16
kiran <kveeramalla@keypoint-tech.com>
2020-02-21 11:58:52 +0530

    Log building image

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 DIMG=$(basename "$(pwd)")
-echo "Building image ${DIMG}:"
 docker kill ${DIMG}
 docker rm ${DIMG}
+echo "Building image ${DIMG}:"
 docker build -t "${DIMG}" .
 docker run --name "${DIMG}" "${DIMG}"
